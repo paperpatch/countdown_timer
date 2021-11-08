a16e86375f194c4a6089b74765b8b8f6e9e02b88
paperpatch <patchen21@gmail.com>
2021-11-08 14:33:04 -0500

    added placeholders and logic framework

---
+++ assets/python/script.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+current = datetime.now()
+print("current time = ", current)
+
+def countdown(time):
+  while time:
+    min
+
+
+# Get New Years Date in time
+# Get Current Date in time
+
+""" Function():
+  while (t):
+    var timedifference = new years date - current date
+    go through each time
+    define months, days, hours, minutes and seconds in time.
+
+    getElementbyId("") months
+    getElementbyId("") days
+    getElementbyId("") hours
+    getElementbyId("") minutes
+    getElementbyId("") seconds
+    
+    iterate every second
+"""
+
